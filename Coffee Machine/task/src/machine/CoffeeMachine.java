325866e9cd1867278078448f7b5b77e9f9f6c1d3
Teo <novak.teo1@gmail.com>
2021-09-20 16:59:41 +0200

    Stage 5/6

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -1,5 +1,7 @@
 package machine;
+
 import java.util.Scanner;
+
 import static java.lang.Math.min;
 import static machine.CoffeeMachine.CoffeeProduct.*;
 
@@ -8,8 +10,11 @@ public class CoffeeMachine {
     private static final String BUY = "buy";
     private static final String FILL = "fill";
     private static final String TAKE = "take";
+    private static final String REMAINING = "remaining";
+    private static final String EXIT = "exit";
 
     private final Scanner scanner;
+    private boolean running = true;
 
     //Amount of water (ml), milk (ml), coffee beans (g),
     // amount of disposable cups and money hold by the machine.
@@ -26,11 +31,10 @@ public class CoffeeMachine {
 
 
     public static void main(String[] args) {
-        CoffeeMachine coffee = new CoffeeMachine(new Scanner(System.in));
-        coffee.info();
-        coffee.action();
-        coffee.info();
-
+        CoffeeMachine coffeeM = new CoffeeMachine(new Scanner(System.in));
+        while (coffeeM.running) {
+            coffeeM.actions();
+        }
     }
 
     public void info() {
@@ -43,9 +47,9 @@ public class CoffeeMachine {
                 "%n", water, milk, beans, disposableCups, money);
     }
 
-    private void action() {
-        System.out.println("Write action (buy, fill, take):");
-        switch (scanner.nextLine()) {
+    private void actions() {
+        System.out.println("Write action (buy, fill, take, remaining, exit):");
+        switch (scanner.next()) {
             case BUY:
                 buy();
                 break;
@@ -55,10 +59,14 @@ public class CoffeeMachine {
             case TAKE:
                 take();
                 break;
+            case REMAINING:
+                remaining();
+                break;
+            case EXIT:
+                exit();
+                break;
             default:
                 System.out.println("Invalid choice. Please try again.");
-                action();
-                break;
         }
     }
 
@@ -81,29 +89,69 @@ public class CoffeeMachine {
 
     private void buy() {
 
-        System.out.println("What do you want to buy 1 - espresso, 2 - latte, 3 - cappuccino:");
+        System.out.println("What do you want to buy 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu: ");
 
         CoffeeProduct product;
-        switch (scanner.nextInt()) {
-            case 1:
+        switch (scanner.next()) {
+            case "1":
                 product = ESPRESSO;
                 break;
-            case 2:
+            case "2":
                 product = LATTE;
                 break;
-            case 3:
+            case "3":
                 product = CAPPUCCINO;
                 break;
+            case "back":
+                return;
             default:
                 System.out.println("Invalid choice.");
                 return;
         }
+
+        boolean enoughWater = water >= product.getWater();
+        boolean enoughMilk = milk >= product.getMilk();
+        boolean enoughBeans = beans >= product.getBeans();
+        boolean enoughCups = disposableCups > 0;
+
+        if (!enoughWater) {
+            System.out.println("Sorry, not enough water!");
+            return;
+        }
+        if (!enoughMilk) {
+            System.out.println("Sorry, not enough milk!");
+            return;
+        }
+        if (!enoughBeans) {
+            System.out.println("Sorry, not enough beans!");
+            return;
+        }
+        if (!enoughCups) {
+            System.out.println("Sorry, not enough cups!");
+            return;
+        }
+
+        System.out.println("I have enough resources, making you a coffee!");
 
         water -= product.getWater();
         milk -= product.getMilk();
         beans -= product.getBeans();
         disposableCups--;
         money += product.getPrice();
+    }
+
+    private void remaining() {
+        System.out.printf("The Coffee machine has:\n" +
+                "%d ml of water\n" +
+                "%d ml of milk\n" +
+                "%d g of coffee beans\n" +
+                "%d disposable cups\n" +
+                "$%d of money\n" +
+                "%n", water, milk, beans, disposableCups, money);
+    }
+
+    private void exit() {
+        running = false;
     }
 
     private int availableCoffee(int water, int milk, int beans) {
